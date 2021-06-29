ebb048412ba5f0784f61e75fd2e07e99451cae01
chun Siwat <siwat.m@20scoops.net>
2021-06-29 18:23:20 +0700

    add node red

--- pub.js
+++ pub.js
@@ -2,7 +2,7 @@ const mqtt = require("mqtt");
 const client = mqtt.connect("mqtt://localhost:1883");
 
 client.on("connect", function () {
-  client.publish("sensor1", "foo", (err) => {
+  client.publish("sensor1", "bar", (err) => {
     if (err) {
       console.log(err);
       process.exit(1);

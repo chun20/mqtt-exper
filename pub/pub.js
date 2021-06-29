e97a09a4b7878ecd6d754dd41d8890a9587ce7ab
Nattapong Harnkwang <wimkung@gmail.com>
2021-06-29 13:09:36 +0700

    mqtt is working

--- pub/pub.js
+++
@@ -1,7 +0,0 @@
-var mqtt = require("mqtt");
-var client = mqtt.connect("mqtt://localhost:1883");
-
-client.on("connect", function () {
-  console.log("connected");
-  client.publish("presence", "AOOOOOOOO");
-});
---
+++ sub.js
@@ -0,0 +1,14 @@
+const mqtt = require("mqtt");
+const client = mqtt.connect("mqtt://localhost:1883");
+
+client.on('connect', function () {
+  client.subscribe('sensor1', function (err) {
+    if (err) console.log(err);
+    console.log('Worker is running...');
+  })
+});
+
+client.on("message", function (topic, message) {
+  // message is Buffer
+  console.log(message.toString());
+});
